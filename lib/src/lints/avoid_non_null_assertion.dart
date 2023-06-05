63d99db7699f242821e66d4f7d5c6bf89f7741fc
Guillaume Roux <rouxguillaume8@gmail.com>
2023-06-05 18:10:38 +0200

    chore: updated lint for the package and added documentation

--- lib/src/lints/avoid_non_null_assertion.dart
+++ lib/src/lints/avoid_non_null_assertion.dart
@@ -2,6 +2,22 @@ import 'package:analyzer/error/error.dart';
 import 'package:analyzer/error/listener.dart';
 import 'package:custom_lint_builder/custom_lint_builder.dart';
 
+/// A lint that reports the use of the "bang" operator (!).
+///
+/// **BAD:**
+/// ```dart
+/// void foo(String? bar) {
+///   print(bar!.length);
+/// }
+/// ```
+///
+/// **GOOD:**
+/// ```dart
+/// void foo(String? bar) {
+///   if (bar == null) throw NullThrownError();
+///   print(bar.length);
+/// }
+/// ```
 class AvoidNonNullAssertion extends DartLintRule {
   AvoidNonNullAssertion() : super(code: _code);
 
