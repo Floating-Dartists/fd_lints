5fc92371a37b14a70427fc92e0f9ac09cd10657c
Guillaume Roux <rouxguillaume8@gmail.com>
2023-06-05 15:23:04 +0200

    chore: created base class for the rule

---
+++ lib/fd_lints.dart
@@ -0,0 +1,19 @@
+import 'package:custom_lint_builder/custom_lint_builder.dart';
+import 'package:fd_lints/src/lints/avoid_non_null_assertion.dart';
+
+PluginBase createPlugin() => _FDLintsPlugin();
+
+class _FDLintsPlugin extends PluginBase {
+  @override
+  List<LintRule> getLintRules(CustomLintConfigs configs) {
+    return [
+      AvoidNonNullAssertion(),
+    ];
+  }
+
+  @override
+  List<Assist> getAssists() {
+    // TODO(TesteurManiak): implement getAssists
+    return [];
+  }
+}
